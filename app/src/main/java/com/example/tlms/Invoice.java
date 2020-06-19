d37e58f3254d7affb26d3ca536e650db7b1cca6f
radhikasarda <radhikasarda296@gmail.com>
2020-06-19 12:24:56 +0530

    Changes as oFunction Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11Function Key 11

--- app/src/main/java/com/example/tlms/Invoice.java
+++ app/src/main/java/com/example/tlms/Invoice.java
@@ -1,26 +1,14 @@
 package com.example.tlms;
 
-import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.EditText;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.Calendar;
-import java.util.Date;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Invoice extends AppCompatActivity implements View.OnClickListener {
+public class Invoice extends AppCompatActivity{
 
     EditText from_date;
     private int mYear, mMonth, mDay, mHour, mMinute;
@@ -35,20 +23,8 @@ public class Invoice extends AppCompatActivity implements View.OnClickListener {
         selected_application_type = bundle.getString("selected_application_type");
         Log.e("APP TYPE---",selected_application_type+"------"+"#######################");
 
-
-
-
-
         setContentView(R.layout.activity_invoice);
-
-        from_date = (EditText) findViewById(R.id.editTextDate);
-
-
-        from_date.setOnClickListener(this);
-        //to_date.setOnClickListener(this);
-
-
-
+        
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.YEAR, 1);
         int nextYearYear = cal.get(Calendar.YEAR);
@@ -72,28 +48,4 @@ public class Invoice extends AppCompatActivity implements View.OnClickListener {
         //}
     }
 
-    @Override
-    public void onClick(View v) {
-
-        //Log.e("V",v+"###################################################################");
-        // Get Current Date
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
-
-            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                    new DatePickerDialog.OnDateSetListener() {
-
-                        @Override
-                        public void onDateSet(DatePicker view, int year,
-                                              int monthOfYear, int dayOfMonth) {
-
-                            from_date.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
-
-                        }
-                    }, mYear, mMonth, mDay);
-            datePickerDialog.show();
-
-    }
 }
